07c37e1c77eb05e931ab2dae4b0fccd3557c95c0
Luka Vrabec <lvrabec.101@gmail.com>
2022-11-07 17:23:40 +0100

    1st commit 1st commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+
+
+
 
 function App() {
+
+  const [count,setCount] = useState(()=>{})
+
+
+ function decrementCount() {
+
+  setCount(prevCount => prevCount - 1);
+ }
+
+ function increaseCount(){
+
+  setCount(prevCount => prevCount + 1);
+ }
+
+
+
+
+
+
+
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <>
+  <button onClick={decrementCount}>-</button>
+  <span>{count}</span>
+  <button onClick={increaseCount}>+</button>
+  <br></br>
+
+  
+  </>
   );
 }
 
